b91b2f4a1e3889391607924100e505208e6f573e
Van Tran <19809586+Van-Code@users.noreply.github.com>
2021-01-19 21:12:58 -0500

    initial commit

---
+++ country-item.js
@@ -0,0 +1,24 @@
+class CountryItem extends HTMLElement {
+  constructor(itm) {
+    super();
+
+    const { alpha2Code, name, capital } = itm;
+
+    const template = document.getElementById("country-item-template").content;
+    const shadowRoot = this.attachShadow({ mode: "open" }).appendChild(
+      template.cloneNode(true)
+    );
+
+    let nameSlot = this.shadowRoot.querySelector("#name");
+    nameSlot.textContent = name;
+
+    const capitalSlot = this.shadowRoot.querySelector("#capital");
+    capitalSlot.textContent = capital;
+
+    this.style = `background-image:url('https://www.countryflags.io/${alpha2Code.toLowerCase()}/flat/64.png') `;
+  }
+  connectedCallback() {
+    this.setAttribute("tabindex", 0);
+  }
+}
+customElements.define("country-item", CountryItem);
---
+++ country-list.js
@@ -0,0 +1,33 @@
+class EuCountryList extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    fetch("https://restcountries.eu/rest/v2/all?fields=alpha2Code;capital;name")
+      .then((resp) => {
+        if (!resp.ok) {
+          throw Error("Err code: " + resp.status);
+        } else {
+          return resp.json();
+        }
+      })
+      .then((data) => {
+        data.forEach((itm) => {
+          const item = new CountryItem(itm);
+          shadow.appendChild(item);
+        });
+      })
+
+      .catch((resp) => {
+        const para = document.createElement("p");
+        para.textContent =
+          "There was a problem retrieving the data. Please try again later.";
+        shadow.appendChild(para);
+      });
+  }
+  connectedCallback() {
+    this.setAttribute("tabindex", 0);
+  }
+}
+customElements.define("eu-country-list", EuCountryList);
